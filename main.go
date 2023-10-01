937c28da4e9d5fcda842155b594a50b6862ede82
Angelo Rosace <accilo@gf-ARos02.local>
2023-10-01 18:01:15 +0200

    remove firebase instance and user mysql

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"anidex_api/googleApp"
-	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,28 +9,8 @@ import (
 )
 
 func main() {
-	// initialize Firebase APP
-	ctx := context.Background()
 
-	app, err := googleApp.BuildApp(ctx)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	// initialize Firebase DB
-	client, err := app.Database(ctx)
-	if err != nil {
-		log.Fatalln("error in creating firebase DB client: ", err)
-	}
-
-	// create ref at path user_scores/:userId
-	ref := client.NewRef("user_scores/" + fmt.Sprint(2))
-
-	if err := ref.Set(context.TODO(), map[string]interface{}{"score": 40}); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("score added/updated successfully!")
+	fmt.Println(os.Getenv("MySQL_URL"))
 
 	// initialize API
 	fiberApp := fiber.New()
