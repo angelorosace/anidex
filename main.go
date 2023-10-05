36cd8df1671e6b8ce94a77c69f2ff04866cc095a
Angelo Rosace <accilo@gf-ARos02.local>
2023-10-05 15:43:40 +0200

    save images on server

--- main.go
+++ main.go
@@ -1,39 +1,106 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"path/filepath"
 
 	DB "anidex_api/db"
-
-	"github.com/gofiber/fiber/v2"
 )
+
+type animalPostRequest struct {
+	Photo []struct {
+	} `json:"photo"`
+	Name        string   `json:"name"`
+	Taxonomy    string   `json:"taxonomy"`
+	Etymology   string   `json:"etymology"`
+	Iucn        []string `json:"iucn"`
+	Geo         string   `json:"geo"`
+	Migration   string   `json:"migration"`
+	Habitat     string   `json:"habitat"`
+	Dimensions  string   `json:"dimensions"`
+	Ds          string   `json:"ds"`
+	Diet        string   `json:"diet"`
+	Description string   `json:"description"`
+}
+
+func getStatus(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("OK")
+}
+
+func parseAnimalRequest(m *multipart.Form) {
+
+	for _, v := range m.File["photo[]"] {
+		fmt.Println(v.Filename, ":", v.Size)
+		file, err := v.Open()
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+
+		// Create directory
+		//dirPath := "/Users/accilo/Desktop/angelo/anidex_api/temp-img"
+		dirPath := "/uploaded_images"
+		if _, err := os.Stat(dirPath); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(dirPath, os.ModePerm)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+		// Create file
+		dst, err := os.Create(filepath.Join(dirPath, filepath.Base(v.Filename)))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer dst.Close()
+		if _, err = io.Copy(dst, file); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Successfully Uploaded Images\n", v.Filename)
+	}
+}
+
+func postAnimal(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	parseAnimalRequest(r.MultipartForm)
+
+}
+
+func setupRoutes(port string) {
+	if port == "" {
+		port = "4000"
+	}
+
+	http.HandleFunc("/", getStatus)
+	http.HandleFunc("/animal", postAnimal)
+	http.ListenAndServe("0.0.0.0:"+port, nil)
+}
 
 func main() {
 
-	// Initialize DB
-	db, err := DB.InitializeDB()
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println("Connection with DB established!")
-	}
-	defer db.Close()
-
-	// initialize API Service
-	fiberApp := fiber.New()
-
-	fiberApp.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
 	port := os.Getenv("PORT")
 
-	if port == "" {
-		port = "3000"
+	// Initialize DB
+	if port != "" {
+		db, err := DB.InitializeDB()
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println("Connection with DB established!")
+		}
+		defer db.Close()
 	}
 
-	log.Fatal(fiberApp.Listen("0.0.0.0:" + port))
+	setupRoutes(port)
 
 }
