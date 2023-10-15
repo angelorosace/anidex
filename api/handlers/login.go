5626a58d11b4c5ec11f3411f6a66b78bb2f0c2b6
Angelo Rosace <accilo@gf-ARos02.local>
2023-10-15 18:34:26 +0200

    debug

--- api/handlers/login.go
+++ api/handlers/login.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	responses "anidex_api/http/responses"
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Credentials struct {
@@ -56,48 +61,45 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if user.UserName == "" {
-		fmt.Println("dio bestia")
+		http.Error(w, "User does not exist", http.StatusNotFound)
+		return
 	}
 
-	fmt.Println("dio cane")
+	//get salt from server
+	salt := os.Getenv("SALT")
 
-	/*
-		//get salt from server
-		salt := os.Getenv("SALT")
+	//add salt to password
+	passwordBytes := []byte(credentials.Password)
+	passwordBytes = append(passwordBytes, []byte(salt)...)
 
-		//add salt to password
-		passwordBytes := []byte(credentials.Password)
-		passwordBytes = append(passwordBytes, []byte(salt)...)
+	// Compare the stored hashed password, with the hashed version of the password that was received
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), passwordBytes); err != nil {
+		// If the two passwords don't match, return a 401 status
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
 
-		// Compare the stored hashed password, with the hashed version of the password that was received
-		if err = bcrypt.CompareHashAndPassword([]byte(user.Password), passwordBytes); err != nil {
-			// If the two passwords don't match, return a 401 status
-			http.Error(w, err.Error(), http.StatusUnauthorized)
-			return
-		}
+	//create JWT token
+	expiresAt := time.Now().Add(time.Hour * 24) // Token expiration time (adjust as needed)
 
-		//create JWT token
-		expiresAt := time.Now().Add(time.Hour * 24) // Token expiration time (adjust as needed)
+	claims := jwt.MapClaims{}
+	claims["username"] = credentials.UserName
+	claims["exp"] = expiresAt.Unix()
 
-		claims := jwt.MapClaims{}
-		claims["username"] = credentials.UserName
-		claims["exp"] = expiresAt.Unix()
+	//send back JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-		//send back JWT token
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(salt))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		tokenString, err := token.SignedString([]byte(salt))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	response, e := responses.CustomResponse(w, tokenString, "User Authenticated", http.StatusOK, "")
+	if e != nil {
+		http.Error(w, e.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		response, e := responses.CustomResponse(w, tokenString, "User Authenticated", http.StatusOK, "")
-		if e != nil {
-			http.Error(w, e.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(response)
-	*/
+	w.Write(response)
 }
