54e62886a57f0be98ea0809a62ae2feaca4f78d5
Angelo Rosace <accilo@gf-ARos02.local>
2023-10-15 18:19:16 +0200

    debug

--- api/handlers/login.go
+++ api/handlers/login.go
@@ -1,15 +1,9 @@
 package handlers
 
 import (
-	responses "anidex_api/http/responses"
-	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
-	"os"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Credentials struct {
@@ -24,6 +18,8 @@ type User struct {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	//get password from body
 	//get username from body
 	var credentials Credentials
@@ -32,71 +28,76 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	//get user with same username from DB
-	db := r.Context().Value("db").(*sql.DB)
 
-	// Query the database
-	query := "SELECT * FROM users WHERE username = ?"
-	rows, err := db.Query(query, credentials.UserName)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
+	fmt.Println(credentials)
 
-	if !rows.Next() {
-		http.Error(w, err.Error(), http.StatusNoContent)
-		return
-	}
+	/*
+		//get user with same username from DB
+		db := r.Context().Value("db").(*sql.DB)
 
-	var user User
-	for rows.Next() {
-		err := rows.Scan(
-			&user.ID,
-			&user.UserName,
-			&user.Password,
-		)
+		// Query the database
+		query := "SELECT * FROM users WHERE username = ?"
+		rows, err := db.Query(query, credentials.UserName)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-	}
+		defer rows.Close()
 
-	//get salt from server
-	salt := os.Getenv("SALT")
+		if !rows.Next() {
+			http.Error(w, err.Error(), http.StatusNoContent)
+			return
+		}
 
-	//add salt to password
-	passwordBytes := []byte(credentials.Password)
-	passwordBytes = append(passwordBytes, []byte(salt)...)
+		var user User
+		for rows.Next() {
+			err := rows.Scan(
+				&user.ID,
+				&user.UserName,
+				&user.Password,
+			)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
 
-	// Compare the stored hashed password, with the hashed version of the password that was received
-	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), passwordBytes); err != nil {
-		// If the two passwords don't match, return a 401 status
-		http.Error(w, err.Error(), http.StatusUnauthorized)
-		return
-	}
+		//get salt from server
+		salt := os.Getenv("SALT")
 
-	//create JWT token
-	expiresAt := time.Now().Add(time.Hour * 24) // Token expiration time (adjust as needed)
+		//add salt to password
+		passwordBytes := []byte(credentials.Password)
+		passwordBytes = append(passwordBytes, []byte(salt)...)
 
-	claims := jwt.MapClaims{}
-	claims["username"] = credentials.UserName
-	claims["exp"] = expiresAt.Unix()
+		// Compare the stored hashed password, with the hashed version of the password that was received
+		if err = bcrypt.CompareHashAndPassword([]byte(user.Password), passwordBytes); err != nil {
+			// If the two passwords don't match, return a 401 status
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
 
-	//send back JWT token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		//create JWT token
+		expiresAt := time.Now().Add(time.Hour * 24) // Token expiration time (adjust as needed)
 
-	tokenString, err := token.SignedString([]byte(salt))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+		claims := jwt.MapClaims{}
+		claims["username"] = credentials.UserName
+		claims["exp"] = expiresAt.Unix()
 
-	response, e := responses.CustomResponse(w, tokenString, "User Authenticated", http.StatusOK, "")
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-		return
-	}
+		//send back JWT token
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	w.Write(response)
+		tokenString, err := token.SignedString([]byte(salt))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response, e := responses.CustomResponse(w, tokenString, "User Authenticated", http.StatusOK, "")
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(response)
+	*/
 }
